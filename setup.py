95b24b99fa67440db4770c68bca3d38fbd105ed1
Bank1999 <49858535+Bank1999@users.noreply.github.com>
2020-08-11 17:56:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,34 @@
-......
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="Bank1999-clusterbootstraps",
+    version="3.0.0",
+    description="??????????",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bank1999/clusterbootstraps",
+    author="Bingkun Lin/Shiyue Shen/Ziyi Zhan/Zizhong Yan",
+    author_email=" ?????????",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["clusterbootstraps"],
+    include_package_data=True,
+    install_requires=["numpy", "pandas","statsmodels.api","prettytable"],
+    entry_points={
+        "console_scripts": [
+            "?????????????",
+        ]
+    },
+)
