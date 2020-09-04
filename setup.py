7594894648417e8804d80a1f8585ca3e72383fb9
Bank1999 <49858535+Bank1999@users.noreply.github.com>
2020-09-04 21:15:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="clusterbootstraps",
-    version="0.1.1",
+    version="0.0.1",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/Bank1999/clusterbootstraps",
