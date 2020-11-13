ff8b1e549404e072caf03da5175ce97c2dd2a956
BingkunLin <49858535+BingkunLin@users.noreply.github.com>
2020-11-13 21:29:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="clusterbootstraps",
-    version="0.0.2",
+    version="0.0.3",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/Bank1999/clusterbootstraps",
